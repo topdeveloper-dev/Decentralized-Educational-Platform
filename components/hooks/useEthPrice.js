23722bea5a2fdd63449920f5d5ba23703fa74ddf
jovanni-dev <jovanni.millers123@outlook.com>
2023-02-13 17:13:27 -0500

    fix_fecther_func

--- components/hooks/useEthPrice.js
+++ components/hooks/useEthPrice.js
@@ -3,7 +3,11 @@ import useSWR from "swr"
 const URL = "https://api.coingecko.com/api/v3/coins/ethereum?localization=false&tickers=false&community_data=false&developer_data=false&sparkline=false"
 export const COURSE_PRICE = 15
 
-
+const fetcher= async url =>{
+    const res = await fetch(url)
+    const json = await res.json()
+    return json.market_data.current_price.usd ?? null
+}
 
 export const useEthPrice = () =>{
     const {data, ...rest} = useSWR(
