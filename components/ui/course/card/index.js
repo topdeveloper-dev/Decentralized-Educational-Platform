dab68e7903f05ee0a505cee6e9120920b7c48176
jovanni-dev <jovanni.millers123@outlook.com>
2023-02-13 17:15:01 -0500

    fix_card_index

--- components/ui/course/card/index.js
+++ components/ui/course/card/index.js
@@ -6,7 +6,17 @@ export default function Card({course, Footer, disabled, state}) {
     return (
       <div
       className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-
+      <div className="flex h-full">
+        <div className="flex-1 h-full next-image-wrapper">
+          <Image
+            className={`object-cover ${disabled && "filter grayscale"}`}
+            src={course.coverImage}
+            layout="responsive"
+            width="200"
+            height="230"
+            alt={course.title}
+          />
+        </div>
         <div className="p-8 pb-4 flex-2">
           <div className="flex items-center">
             <div
